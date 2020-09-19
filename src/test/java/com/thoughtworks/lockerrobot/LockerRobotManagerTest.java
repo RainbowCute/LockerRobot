3dcb26c90254b053b2ff7bbfbd4e70001d963b18
zhoumo <mo.zhou@thoughtworks.com>
2020-09-19 16:54:29 +0800

    [zhoumo] N/A feat: return ticket and bag in L type locker when namager save L type bag

--- src/test/java/com/thoughtworks/lockerrobot/LockerRobotManagerTest.java
+++ src/test/java/com/thoughtworks/lockerrobot/LockerRobotManagerTest.java
@@ -41,4 +41,18 @@ public class LockerRobotManagerTest {
         assertNotNull(ticket);
         assertEquals(bag, primaryLockerRobot.take(ticket));
     }
+
+    @Test
+    public void should_return_ticket_and_bag_in_L_type_locker_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_L_type_bag_and_common_customer() {
+        Locker locker = new Locker(10, Type.S);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(10, Type.M)));
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Collections.singletonList(new Locker(10, Type.L)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker, primaryLockerRobot, superLockerRobot));
+        Bag bag = new Bag(Type.L);
+
+        Ticket ticket = lockerRobotManager.save(bag);
+
+        assertNotNull(ticket);
+        assertEquals(bag, superLockerRobot.take(ticket));
+    }
 }
