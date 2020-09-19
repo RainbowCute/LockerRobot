46567e842ce14fec78f28890333b86fa238e5fbe
zhoumo <mo.zhou@thoughtworks.com>
2020-09-19 16:57:54 +0800

    [zhoumo] #N/A feat: throw full capacity exception when manager save bag

--- src/test/java/com/thoughtworks/lockerrobot/LockerRobotManagerTest.java
+++ src/test/java/com/thoughtworks/lockerrobot/LockerRobotManagerTest.java
@@ -44,7 +44,7 @@ public class LockerRobotManagerTest {
     }
 
     @Test
-    public void should_return_ticket_and_bag_in_L_type_locker_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_L_type_bag_and_common_customer() {
+    public void should_return_ticket_and_bag_in_L_type_locker_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_L_type_bag_and_VIP_customer() {
         Locker locker = new Locker(10, Type.S);
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(10, Type.M)));
         SuperLockerRobot superLockerRobot = new SuperLockerRobot(Collections.singletonList(new Locker(10, Type.L)));
@@ -58,13 +58,37 @@ public class LockerRobotManagerTest {
     }
 
     @Test(expected = FullCapacityException.class)
-    public void should_throw_full_capacity_exception_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_and_S_type_locker_has_no_free_capacity_and_S_type_bag_and_common_customer() {
+    public void should_throw_full_capacity_exception_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_and_S_type_locker_has_no_free_capacity_and_S_type_bag_and_VIP_customer() {
         Locker locker = new Locker(1, Type.S);
         locker.save(new Bag());
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(10, Type.M)));
         SuperLockerRobot superLockerRobot = new SuperLockerRobot(Collections.singletonList(new Locker(10, Type.L)));
         LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker, primaryLockerRobot, superLockerRobot));
         Bag bag = new Bag(Type.S);
+
+        lockerRobotManager.save(bag);
+    }
+
+    @Test(expected = FullCapacityException.class)
+    public void should_throw_full_capacity_exception_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_and_M_type_locker_has_no_free_capacity_and_M_type_bag_and_VIP_customer() {
+        Locker locker = new Locker(10, Type.S);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(1, Type.M)));
+        primaryLockerRobot.save(new Bag());
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Collections.singletonList(new Locker(10, Type.L)));
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker, primaryLockerRobot, superLockerRobot));
+        Bag bag = new Bag(Type.M);
+
+        lockerRobotManager.save(bag);
+    }
+
+    @Test(expected = FullCapacityException.class)
+    public void should_throw_full_capacity_exception_when_locker_robot_manager_save_bag_given_3_types_of_locker_and_and_L_type_locker_has_no_free_capacity_and_L_type_bag_and_VIP_customer() {
+        Locker locker = new Locker(10, Type.S);
+        PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Collections.singletonList(new Locker(10, Type.M)));
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Collections.singletonList(new Locker(1, Type.L)));
+        superLockerRobot.save(new Bag());
+        LockerRobotManager lockerRobotManager = new LockerRobotManager(Arrays.asList(locker, primaryLockerRobot, superLockerRobot));
+        Bag bag = new Bag(Type.L);
 
         lockerRobotManager.save(bag);
     }
