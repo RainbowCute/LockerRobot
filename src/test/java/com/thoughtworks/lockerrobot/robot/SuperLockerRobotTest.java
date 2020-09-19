b5abbee9bbb9c8f6105bdc9f947d8a526cb28cef
zhoumo <mo.zhou@thoughtworks.com>
2020-09-19 12:28:15 +0800

    [zhoumo] #N/A feat: return ticket and bag in more free capacity locker when robot save bag

--- src/test/java/com/thoughtworks/lockerrobot/robot/SuperLockerRobotTest.java
+++ src/test/java/com/thoughtworks/lockerrobot/robot/SuperLockerRobotTest.java
@@ -5,9 +5,11 @@ import com.thoughtworks.lockerrobot.Locker;
 import com.thoughtworks.lockerrobot.Ticket;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SuperLockerRobotTest {
 
@@ -20,5 +22,19 @@ public class SuperLockerRobotTest {
         Ticket ticket = superLockerRobot.save(bag);
 
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_return_ticket_and_save_bag_in_1st_locker_when_super_locker_robot_save_bag_given_1st_locker_with_more_free_capacity_rate_and_bag() {
+        Locker firstLocker = new Locker(10);
+        Locker secondLocker = new Locker(5);
+        secondLocker.save(new Bag());
+        SuperLockerRobot superLockerRobot = new SuperLockerRobot(Arrays.asList(firstLocker, secondLocker));
+        Bag bag = new Bag();
+
+        Ticket ticket = superLockerRobot.save(bag);
+
+        assertNotNull(ticket);
+        assertEquals(bag, firstLocker.take(ticket));
     }
 }
